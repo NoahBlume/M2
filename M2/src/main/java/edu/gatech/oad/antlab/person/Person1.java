194053f5778f27f3b104167addb8dfe1ce1560da
Noah Blume <noahablume@gmail.com>
2017-01-24 17:47:22 -0500

    added calc method to person1.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,22 +3,22 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
- *  @author Bob
+ *  modified string
+ *
+ *  @author Noah Blume
  *  @version 1.1
  */
 public class Person1 {
-  /** Holds the persons real name */
-  private String name;
+    /** Holds the persons real name */
+    private String name;
   	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-  public Person1(String pname) {
-    name = pname;
-  }
+    public Person1(String pname) {
+        name = pname;
+    }
   	/**
 	 * This method should take the string
 	 * input and return its characters rotated
@@ -30,20 +30,24 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	    //Person 1 put your implementation here
+		String newString = "";
+		for(int i = 2; i < input.length() + 2; i++) {
+			newString += input[i % input.length()];
+		}
+		return newString;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+	    return name + calc(input);
 	}
 
 }
